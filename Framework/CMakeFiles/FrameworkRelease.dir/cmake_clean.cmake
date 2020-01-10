0a0a4dd96b56a1dc6715219abadd5129c9889938
mareux <mareuxbaka@gmail.com>
2020-01-10 19:29:50 +0200

    1

---
+++ Framework/CMakeFiles/FrameworkRelease.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FrameworkRelease.dir/src/launch.cpp.o"
+  "libFrameworkRelease.0.0.1.dylib"
+  "libFrameworkRelease.dylib"
+  "libFrameworkRelease.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FrameworkRelease.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
